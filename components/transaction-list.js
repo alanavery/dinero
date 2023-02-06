8f1a09a71d2bfb80ed7e85aa9dc05709b87e1a4a
Alan Avery <delayedaa@gmail.com>
2023-02-06 11:06:51 -0600

    add ability to toggle status of transactions

--- components/transaction-list.js
+++ components/transaction-list.js
@@ -1,11 +1,14 @@
 import { useContext } from 'react';
+import axios from 'axios';
 import UserContext from '@/store/user-context';
 import { calculateBalance } from '@/helpers/balance-utils';
 
 const TransactionList = () => {
   const {
+    userId,
     activeAccount,
     transactionData: { transactions, payees, tags },
+    setTransactionData,
   } = useContext(UserContext);
 
   const accountTransactions = transactions.filter((transaction) => transaction.accountId === activeAccount._id);
@@ -21,8 +24,30 @@ const TransactionList = () => {
               const payee = payees.find((payee) => payee._id === transaction.payeeId);
               const tag = tags.find((tag) => tag._id === transaction.tagId);
 
+              const toggleStatus = async () => {
+                const formData = {
+                  toggleStatus: true,
+                  userId,
+                  cleared: transaction.cleared ? false : true,
+                };
+
+                await axios
+                  .post(`/api/transactions/${transaction._id}`, formData)
+                  .then((response) => {
+                    console.log(response);
+                    setTransactionData(response.data.transactionData);
+                  })
+                  .catch((error) => {
+                    console.log(error);
+                    setMessage(error.response.data.message);
+                  });
+              };
+
               return (
                 <li className={transaction.cleared ? 'cleared' : undefined} key={transaction._id}>
+                  <form>
+                    <input type="checkbox" checked={transaction.cleared} onChange={toggleStatus} />
+                  </form>
                   <div>{transaction.date}</div>
                   <div>{payee && payee.name}</div>
                   <div>{`$${transaction.amount}`}</div>
