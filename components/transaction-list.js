7d9d0f457c07e9e17acb0e4ff8c3a2ef7012b525
Alan Avery <delayedaa@gmail.com>
2023-04-11 23:45:38 -0500

    group transactions by date

--- components/transaction-list.js
+++ components/transaction-list.js
@@ -6,52 +6,80 @@ const TransactionList = ({ userId, accountId, account, transactions, payees, tag
   const [showCleared, setShowCleared] = useState(false);
 
   const accountTransactions = transactions.filter((transaction) => transaction.accountId === accountId);
-  const sortedAccountTransactions = accountTransactions.sort((a, b) => new Date(b.date) - new Date(a.date));
+  const sortedTransactions = accountTransactions.sort((a, b) => new Date(b.date) - new Date(a.date));
+  const transactionGroups = [];
+  let currentDate = '';
+
+  sortedTransactions.forEach((transaction) => {
+    if (transaction.date !== currentDate) {
+      const group = {
+        date: transaction.date,
+        transactions: [transaction],
+      };
+      transactionGroups.push(group);
+      currentDate = transaction.date;
+    } else {
+      const groupIndex = transactionGroups.findIndex((group) => group.date === transaction.date);
+      transactionGroups[groupIndex].transactions.push(transaction);
+    }
+  });
+
+  console.log(sortedTransactions);
+  console.log(transactionGroups);
 
   return (
     <section className="transactions">
       {accountTransactions.length >= 1 && (
         <>
-          <div className="transactions__summary">
+          <div className="card card--lg">
             <div>
-              <div>Account Balance:</div>
-              <div>{`$${calculateBalance(account.startingBalance, accountTransactions)}`}</div>
+              <div className="card__body--lg">{`$${calculateBalance(account.startingBalance, accountTransactions)}`}</div>
+              <div className="card__heading">Account Balance</div>
             </div>
 
             <div>
-              <div>Cleared Balance:</div>
-              <div>{`$${calculateBalance(account.startingBalance, accountTransactions, 'cleared')}`}</div>
+              <div className="card__body--lg">{`$${calculateBalance(account.startingBalance, accountTransactions, 'cleared')}`}</div>
+              <div className="card__heading">Cleared Balance</div>
             </div>
           </div>
 
-          <ul>
-            {sortedAccountTransactions.map((transaction) => {
-              const payee = payees.find((payee) => payee._id === transaction.payeeId);
-              const tag = tags.find((tag) => tag._id === transaction.tagId);
+          {transactionGroups.map((group) => {
+            return (
+              <div className="group" key={group.date}>
+                <div className="group__date">{group.date}</div>
 
-              if (showCleared || (!showCleared && !transaction.cleared)) {
-                return (
-                  <li key={transaction._id}>
-                    <div className="list__item__text">
-                      <div>{transaction.date}</div>
-                      <div className={transaction.split ? 'split' : false}>{payee && payee.name}</div>
-                      <div>{`$${transaction.amount.toFixed(2)}`}</div>
-                      {transaction.tag && <div>{tag && tag.name}</div>}
-                    </div>
+                <ul className="card card--sm">
+                  {group.transactions.map((transaction) => {
+                    const payee = payees.find((payee) => payee._id === transaction.payeeId);
+                    const tag = tags.find((tag) => tag._id === transaction.tagId);
 
-                    <div className="list__item__buttons">
-                      <Link className="button" href={`/users/${userId}/accounts/${accountId}/transactions/${transaction._id}/edit`}>
-                        Edit
-                      </Link>
-                      <Link className="button" href={`/users/${userId}/accounts/${accountId}/transactions/${transaction._id}/delete`}>
-                        Delete
-                      </Link>
-                    </div>
-                  </li>
-                );
-              }
-            })}
-          </ul>
+                    if (showCleared || (!showCleared && !transaction.cleared)) {
+                      return (
+                        <li key={transaction._id}>
+                          <div className="list__item__text">
+                            {/* <div>{transaction.date}</div> */}
+                            <div className={transaction.split ? 'split' : false}>{payee && payee.name}</div>
+                            <div>{`$${transaction.amount.toFixed(2)}`}</div>
+                            {transaction.tag && <div>{tag && tag.name}</div>}
+                          </div>
+
+                          <div className="list__item__buttons">
+                            <Link className="button" href={`/users/${userId}/accounts/${accountId}/transactions/${transaction._id}/edit`}>
+                              Edit
+                            </Link>
+
+                            <Link className="button" href={`/users/${userId}/accounts/${accountId}/transactions/${transaction._id}/delete`}>
+                              Delete
+                            </Link>
+                          </div>
+                        </li>
+                      );
+                    }
+                  })}
+                </ul>
+              </div>
+            );
+          })}
 
           <button onClick={() => setShowCleared(showCleared ? false : true)}>{showCleared ? 'Hide Cleared' : 'Show Cleared'}</button>
         </>
